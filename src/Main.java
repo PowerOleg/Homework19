8dc2a40469f66efd51d75f2d9f375f2c00d56c2b
PowerOleg <geraskin.oleg@mail.ru>
2022-11-14 00:30:50 +0500

    steamData saved!

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
-//отфильтровать через стрим не буквы
+//override equals() and hashCode()
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Main {
     private static String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
@@ -11,16 +13,17 @@ public class Main {
             value++;
             hashMap.put(ch, value);
     }
-    public static void print(Map<Character, Integer> hashMap) {
-        for (Map.Entry<Character, Integer> e : hashMap.entrySet()) {
-            System.out.println(e);
-        }
-    }
+//    public static void cleanMap(Map<Character, Integer> hashMap) {
+//
+//}
+//    public static int findTheBiggest(Map<Character, Integer> hashMap2, int min) {
+//
+//        return min;
+//    }
 
     public static void main(String[] args) {
         Map<Character, Integer> hashMap = new HashMap<>();
         Iterator<Integer> iterator = text.chars().iterator();
-
 
         while (iterator.hasNext()) {
             int i = iterator.next();
@@ -29,13 +32,18 @@ public class Main {
             if (!hashMap.containsKey(ch)) hashMap.put(ch, 1);
             else containsKey(hashMap, ch);
         }
-        hashMap.entrySet().stream().filter(n -> Character.isLetter(n.getKey()))/*.map(Map.Entry::getKey)*/.forEach(System.out::println);
+
+        List<Map.Entry<Character, Integer>> hashMap1 =
+                hashMap.entrySet().stream().filter(n -> Character.isLetter(n.getKey())).collect(Collectors.toList());
+        int min = Integer.MIN_VALUE;
+        int max = Integer.MAX_VALUE;
+        for (Map.Entry<Character, Integer> e : hashMap1) {
+//            if (e.getValue() > min) {
+//                min = e.getValue();
+//            }
+            System.out.println(e);
 
 
-
-
-//        print(hashMap);
-
-
+        }
     }
 }
